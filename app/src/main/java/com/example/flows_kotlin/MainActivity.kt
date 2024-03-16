aefd02b1b6ec01d0868afcd44d2accc2b594b6e1
klyushkin <stepan.9h88@mail.ru>
2024-03-16 21:28:05 +0300

    StateFlow in XML

--- app/src/main/java/com/example/flows_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/flows_kotlin/MainActivity.kt
@@ -16,13 +16,30 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.flows_kotlin.ui.theme.FlowskotlinTheme
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+
+        lifecycleScope.launch{
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.stateFlow.collectLatest { number ->
+                    binding.tvCounter.text = number.toString()
+                }
+            }
+        }
+
+        /*setContent {
             FlowskotlinTheme {
                 val viewModel = viewModel<MainViewModel>()
                 val count = viewModel.stateFlow.collectAsState(initial = 10)
@@ -33,6 +50,6 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
-        }
+        }*/
     }
 }
